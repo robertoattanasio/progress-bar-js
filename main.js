fab78a30aa547f2a57f5befcca239ab4fe9459a9
Roberto <r.attanasio90@gmail.com>
2021-06-03 13:44:01 +0200

    completed

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// function start() {
-//     var fillProgressBar = document.querySelector('.progress-bar-fill');
-//     var progressbarWidth = 0;
-//     var progressBar = setInterval(progressBar, 10);
-
-//     function progressBar() {
-//         if (progressbarWidth == 100) {
-//             clearInterval(progressBar);
-
-//         } else {
-//             progressbarWidth++;
-//             fillProgressBar.style.width = progressbarWidth + '%';
-//         }
-//     }
-// }
-
 function progressBar() {
     var emptyProgressBar = document.querySelector('.progress-bar');
     var filledProgressBar = document.querySelector('.progress-bar-fill');
